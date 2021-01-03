b9bbaa9eeae94af8cbd109517ab45909d3e13f10
jason <jasonabisaad@hotmail.com>
2021-01-03 20:31:06 +0200

    Initial Commit

--- app/src/main/java/com/example/ewallet/QrScanner.java
+++ app/src/main/java/com/example/ewallet/QrScanner.java
@@ -2,55 +2,111 @@ package com.example.ewallet;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.os.Vibrator;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
-import com.budiyev.android.codescanner.CodeScanner;
-import com.budiyev.android.codescanner.CodeScannerView;
-import com.budiyev.android.codescanner.DecodeCallback;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.zxing.Result;
 
+import java.io.IOException;
+
 public class QrScanner extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
+    SurfaceView surfaceView;
+    CameraSource cameraSource;
+    TextView textView;
+    BarcodeDetector barcodeDetector;
+    ImageView backimg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scanner);
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+        surfaceView = findViewById(R.id.camerapreview);
+        textView = findViewById(R.id.textView);
+        backimg = findViewById(R.id.backimg_scanner);
+        backimg.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDecoded(@NonNull final Result result) {
-                runOnUiThread(new Runnable() {
+            public void onClick(View v) {
+                backHome();
+            }
+        });
+
+        barcodeDetector = new BarcodeDetector.Builder(this).
+
+                setBarcodeFormats(Barcode.QR_CODE).
+
+                build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector).
+
+                setRequestedPreviewSize(640, 480).
+
+                setAutoFocusEnabled(true).
+
+                build();
+        surfaceView.getHolder().
+
+                addCallback(new SurfaceHolder.Callback() {
                     @Override
-                    public void run() {
-                        Toast.makeText(QrScanner.this, result.getText(), Toast.LENGTH_SHORT).show();
-                        //SendFragment.send_address.setText(result.getText());
-                        //finish();
+                    public void surfaceCreated(SurfaceHolder holder) {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                            return;
+                        try {
+                            cameraSource.start(holder);
+                        } catch (IOException e) {
+
+                        }
+                    }
+
+                    @Override
+                    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+                    }
+
+                    @Override
+                    public void surfaceDestroyed(SurfaceHolder holder) {
+                        cameraSource.stop();
                     }
                 });
-            }
-        });
-        scannerView.setOnClickListener(new View.OnClickListener() {
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
             @Override
-            public void onClick(View view) {
-                mCodeScanner.startPreview();
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+                if (qrCodes.size() != 0) {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                            vibrator.vibrate(1000);
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
             }
         });
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mCodeScanner.startPreview();
-    }
-
-    @Override
-    protected void onPause() {
-        mCodeScanner.releaseResources();
-        super.onPause();
+    private void backHome() {
+        finish();
     }
 }
