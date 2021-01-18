cfbf67f6108dcf3a049a9e90ef34b85bc7e91a81
Elie Aoun <eaoun.2001@outlook.com>
2021-01-18 14:05:06 +0200

    -Help fragment -> Onpostexecute( ma tensa tred l  IP la 10.0.2.2 )

--- app/src/main/java/com/example/ewallet/HelpFragment.java
+++ app/src/main/java/com/example/ewallet/HelpFragment.java
@@ -1,12 +1,17 @@
 package com.example.ewallet;
 
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +19,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -34,7 +54,7 @@ public class HelpFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    private EditText message;
+    private EditText messageEditText;
     private Button sendMessage;
 
     public HelpFragment() {
@@ -74,17 +94,92 @@ public class HelpFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.help_layout, container, false);
 
-        message = (EditText)v.findViewById(R.id.message);
+        messageEditText = (EditText)v.findViewById(R.id.message);
         sendMessage = (Button)v.findViewById(R.id.sendMessage);  //send button
-        sendMessage.setOnClickListener(new View.OnClickListener() {   //send button listener
+        sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String help_message = message.getText().toString();
+                String help_message = messageEditText.getText().toString();
                 if(help_message.isEmpty()){
-                    Toast.makeText(getActivity(),"Please enter a message !", Toast.LENGTH_SHORT).show();
-                }else {
+                    Toast.makeText(getActivity(),"Please enter a message",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                new HelpMsg().execute(help_message);
+            }
+        });
+        return v ;
+    }
+
+
+    private class HelpMsg extends AsyncTask<String, String , String> {
+        ProgressDialog pdLoading = new ProgressDialog(getActivity());
+        HttpURLConnection conn;
+        URL url = null;
+        public static final int CONNECTION_TIMEOUT = 10000;
+        public static final int READ_TIMEOUT = 15000;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // this method runs on same UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                url = new URL("http://192.168.1.71/cryptoBank/helpMessage.inc.php");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "exception1";
+            }
+            try {
+                //here we will setup HttpURLConnection to connect with php scripts to send and receive data
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");//the request method must correspond with the written php code
+
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                //append parameters to url so that the script uses them
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("message", params[0]);//params[0] is the message from AsyncLogin().execute(help_message);
+                String query = builder.build().getEncodedQuery();
+                OutputStream os;
+                os = conn.getOutputStream();//Open connection
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                if (query != null)
+                    writer.write(query);//write the formed query to the output stream
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                Log.d("HOSTINGG", e1.getMessage());
+                Log.d("HOSTING", Arrays.toString(e1.getStackTrace()));
+                Log.d("HOSTING", "OK");
+                return "exception2";
+            }
+
+            try {
+                int response_code = conn.getResponseCode();
+                if (response_code == HttpURLConnection.HTTP_OK) {
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);//the result here will be what the echo from the php script
+                    }
+                    return result.toString();//result will be used in onPostExecute method
+                } else {
                     AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-                    builder.setMessage("Thank you for contacting us !\n Please wait 24 hours before sending another  message.");
+                    builder.setMessage("Connection Failed");
                     builder.setCancelable(false);
                     builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
                         @Override
@@ -94,22 +189,99 @@ public class HelpFragment extends Fragment {
                     });
                     AlertDialog dialog = builder.create();
                     dialog.show();
-                    message.setText("");
-                    message.setEnabled(false);
-
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            message.setEnabled(true);
-                        }
-                    }, 86400000);
+                    return "unsuccesfull";
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception3";
+            } finally {
+                conn.disconnect();
             }
-        });
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
 
 
+            if(result.equalsIgnoreCase("true")) {
+                builder.setMessage("Thank you for contacting us !\n Please wait 24 hours before sending another  message.");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                messageEditText.setText("");
+                messageEditText.setEnabled(false);
 
-        return v ;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        messageEditText.setEnabled(true);
+                    }
+                }, 86400000);
+
+            } else if(result.equalsIgnoreCase("false")){
+                builder.setMessage("Data insertion failed");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            } else {
+                builder.setMessage("Unknown error");
+                builder.setCancelable(false);
+                builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
+            }
+        }
+
     }
-
 }
+
+
+
+   /* String help_message = message.getText().toString();
+                if(help_message.isEmpty()){
+                        Toast.makeText(getActivity(),"Please enter a message !", Toast.LENGTH_SHORT).show();
+                        }else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                        builder.setMessage("Thank you for contacting us !\n Please wait 24 hours before sending another  message.");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+@Override
+public void onClick(DialogInterface dialog, int which) {
+        dialog.cancel();
+        }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        message.setText("");
+        message.setEnabled(false);
+
+        new Handler().postDelayed(new Runnable() {
+@Override
+public void run() {
+        message.setEnabled(true);
+        }
+        }, 86400000); */
